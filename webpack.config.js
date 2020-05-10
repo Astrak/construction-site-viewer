27d9e3fa1ceb2ee8e334dfad94a24b9ebe874027
Atrahasis <coffre.a@gmail.com>
2020-05-10 03:11:34 +0200

    Update dev tools - Remove yarn - Place bundle at root - Recommend beautify - Unify config file for prod and dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,16 @@
-module.exports = {
+const path = require("path");
+const config = {
     entry: "./src/index.ts",
     output: {
         filename: "bundle.js",
-        publicPath: "",
+        path: path.resolve(__dirname),
     },
     resolve: {
         extensions: [".ts", ".tsx", ".js"],
     },
-    devtool: "source-map",
     module: {
         rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "ts-loader",
-            },
+            { test: /\.tsx?$/, loader: "ts-loader" },
             {
                 test: /\.css$/,
                 loader:
@@ -21,4 +18,31 @@ module.exports = {
             },
         ],
     },
+    plugins: [],
+};
+
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === "production";
+    config.mode = isProduction ? "production" : "development";
+    if (isProduction) {
+        const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+        const CompressionPlugin = require("compression-webpack-plugin");
+        const uglify = new UglifyJsPlugin({
+            uglifyOptions: {
+                ie8: false,
+                ecma: 8,
+                output: {
+                    comments: false,
+                    beautify: false,
+                },
+                compress: true,
+                warnings: false,
+            },
+        });
+        const gzip = new CompressionPlugin();
+        config.plugins.push(...[uglify, gzip]);
+    } else {
+        config.devtool = "eval-source-map";
+    }
+    return config;
 };
